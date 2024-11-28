46f39682ecd8aa6548fde708c040d72f2adadb69
Felipe Valiati <felipe.cvaliati@gmail.com>
2024-11-28 15:48:36 -0300

    chore added comments and changed default values

--- App.java
+++ App.java
@@ -1,8 +1,8 @@
 
 public class App {
     public static int solutions;
-    public static final boolean CLOSED_LAP = true; 
-    public static final int SIZE = 6;
+    public static final boolean CLOSED_LAP = true; // change this to allow open laps
+    public static final int SIZE = 5; // change this to edit the board's size
     
     public static void main(String[] args) {
         long st = System.currentTimeMillis();
@@ -14,12 +14,13 @@ public class App {
     }
 
     public static void solve(int x, int y, int last, int[][] board) {
-        if (!Utilities.validPosition(x, y, board)) return;
-        if (board[x][y] != 0) return;
-        if (last == 2 && x == 1 && y == 2) return;
+        if (!Utilities.validPosition(x, y, board)) return; // if current position is out of bounds
+        if (board[x][y] != 0) return; // if current position is already visited
+        if (last == 2 && x == 1 && y == 2) return; // if current position is the mirror of the first position
 
-        board[x][y] = last;
+        board[x][y] = last; // marks current position with the number it was visited on
 
+        // if this is the last number to put, marks as solution
         if (last == board.length * board[0].length && (!CLOSED_LAP || Utilities.laps(x, y, board))) {
             Utilities.printBoard(board);
 
@@ -29,10 +30,11 @@ public class App {
             return;
         }
 
+        //explores all possible moves
         for (int[] move : Utilities.moves) {
             solve(x + move[0], y + move[1], last + 1, board);
         }
 
-        board[x][y] = 0;
+        board[x][y] = 0; // unmarks current position
     }
 }

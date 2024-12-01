81d26092b398deb18bbb780be97c75ab932d51e6
Felipe Valiati <felipe.cvaliati@gmail.com>
2024-12-01 15:37:53 -0300

    perf warnsdorf's rule

--- App.java
+++ App.java
@@ -1,21 +1,19 @@
 public class App {
-    public static int solutions;
-    public static final boolean CLOSED_LAP = true; // change this to allow open laps
-    public static final int SIZE = 6; // change this to edit the board's size
-    
+    public static final boolean CLOSED_LAP = false; // change this to allow open laps
+    public static final int SIZE = 8; // change this to edit the board's size
     public static void main(String[] args) {
         long st = System.currentTimeMillis();
 
         int[][] board = new int[SIZE][SIZE];
-        solve(0, 0, 1, board);
+        int solutions = solve(0, 0, 1, board);
 
         System.out.println("Found " + solutions + " geometrically distincts solutions after " + (System.currentTimeMillis() - st)/1000 + "s");
     }
 
-    public static void solve(int x, int y, int last, int[][] board) {
-        if (!Utilities.validPosition(x, y, board)) return; // if current position is out of bounds
-        if (board[x][y] != 0) return; // if current position is already visited
-        if (last == 2 && x == 1 && y == 2) return; // if current position is the mirror of the first position
+    public static int solve(int x, int y, int last, int[][] board) {
+        if (last == 2 && x == 1 && y == 2) return 0; // if current position is the mirror of the first position
+        if (!Utilities.validPosition(x, y, board)) return 0; // if current position is out of bounds
+        if (board[x][y] != 0) return 0; // if current position is already visited
 
         board[x][y] = last; // marks current position with the number it was visited on
 
@@ -23,17 +21,20 @@ public class App {
         if (last == board.length * board[0].length && (!CLOSED_LAP || Utilities.laps(x, y, board))) {
             Utilities.printBoard(board);
 
-            solutions++;
-
             board[x][y] = 0;
-            return;
+            return 1;
         }
 
+        int[][] sorted = Utilities.sortBestMoves(board, new int[] {x, y});
+
         //explores all possible moves
-        for (int[] move : Utilities.moves) {
-            solve(x + move[0], y + move[1], last + 1, board);
+        int solutions = 0;
+        for (int[] move : sorted) {
+            solutions += solve(x + move[0], y + move[1], last + 1, board);
         }
 
         board[x][y] = 0; // unmarks current position
+
+        return solutions;
     }
 }
--- Utilities.java
+++ Utilities.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Utilities {
     public static int[][] moves = {
         {1, 2},
@@ -9,16 +11,6 @@ public class Utilities {
         {-2, 1},
         {-2, -1}
     };
-
-    public static void printBoard(int[][] board) {
-        System.out.println("Solution: ");
-        for (int[] row : board) {
-            for (int cell : row) {
-                System.out.print(cell + (" ".repeat(2 - ((int) Math.log10(cell)))));
-            }
-            System.out.println();
-        }
-    }
 
     public static boolean validPosition(int x, int y, int[][] board) {
         return x >= 0 && x < board.length && y >= 0 && y < board[0].length;
@@ -35,5 +27,55 @@ public class Utilities {
         }
 
         return false;
+    }
+
+    // sorts all moves by the number of possible moves that can be made from that position (Warnsdorf's rule)
+    public static int[][] sortBestMoves(int[][] board, int[] pos) {
+        int[][] sorted = moves.clone();
+
+        Arrays.sort(sorted, (moveA, moveB) -> {
+            int aPoints = getPossibleMovesForPosition(pos[0] + moveA[0], pos[1] + moveA[1], board);
+            int bPoints = getPossibleMovesForPosition(pos[0] + moveB[0], pos[1] + moveB[1], board);
+
+            return aPoints - bPoints;
+        });
+
+        return sorted;
+    }
+
+    public static int getPossibleMovesForPosition(int x, int y, int[][] board) {
+        int possibleMoves = 0;
+
+        for (int[] move : moves) {
+            int newX = x + move[0];
+            int newY = y + move[1];
+
+            if (validPosition(newX, newY, board) && board[newX][newY] == 0) {
+                possibleMoves++;
+            }
+        }
+
+        return possibleMoves;
+    }
+
+    public static void printBoard(int[][] board) {
+        System.out.println("Solution: ");
+        int minSpaces = (int) Math.log10(board.length * board[0].length) + 2;
+        for (int[] row : board) {
+            for (int cell : row) {
+                //System.out.print(cell + " ");
+                System.out.print(cell + (" ".repeat(Math.max(0, minSpaces - String.valueOf(cell).length()))));
+            }
+            System.out.println();
+        }
+    }
+
+    public static void printIntMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int cell : row) {
+                System.out.print(cell + " ");
+            }
+            System.out.println();
+        }
     }
 }
